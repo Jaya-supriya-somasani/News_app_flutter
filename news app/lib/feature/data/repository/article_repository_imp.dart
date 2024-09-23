e2c0d8aa7035112a289af2b09d41f95e20788ef5
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-09-23 18:32:28 +0530

    fetch api call

--- news app/lib/feature/data/repository/article_repository_imp.dart
+++ news app/lib/feature/data/repository/article_repository_imp.dart
@@ -1,10 +1,39 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
 import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/feature/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app/feature/data/models/article.dart';
 import 'package:news_app/feature/domain/repositories/article_repository.dart';
 
 class ArticleRepositoryImp implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImp(this._newsApiService);
+
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+            DioError(
+                error: httpResponse.response.statusMessage,
+                response: httpResponse.response,
+                type: DioExceptionType.unknown,
+                requestOptions: httpResponse.response.requestOptions
+            )
+        );
+      }
+    } on DioError catch (e) {
+      return DataFailed(e);
+    }
   }
 }
