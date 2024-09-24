a8f7ec32bb0499e1dfb852335c8e2f41ffc19ed8
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-09-24 14:30:11 +0530

    sql lite imports added

--- news app/lib/feature/data/data_sources/local/app_database.dart
+++ news app/lib/feature/data/data_sources/local/app_database.dart
@@ -1,6 +1,8 @@
 import 'package:floor/floor.dart';
 import 'package:news_app/feature/data/data_sources/local/dao/article_dao.dart';
 import 'package:news_app/feature/data/models/article.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'dart:async';
 part 'app_database.g.dart';
 
 @Database(version: 1, entities: [ArticleModel])
